8dedcd88f6f15bde0e658fba9ddb8a0ffbd86065
ChungNguyenVr912 <chungdeser@gmail.com>
2023-03-08 13:30:57 +0700

    add mouse-event-practice

---
+++ condition-2/mouse-event-moveimg.js
@@ -0,0 +1,21 @@
+let myimage = null;
+let i;
+myimage = document.getElementById('myimage');
+function init()
+{
+    i=1;
+    myimage = document.getElementById('myimage');
+    myimage.setAttribute("style", "position: relative; left: 0px; width: 20%");
+    // myimage.style.position = 'relative';
+    // myimage.style.left = '0px';
+}
+function moveright()
+{
+    if(i<10)
+    {
+        myimage.style.left = parseInt(myimage.style.left) + 50 + 'px';
+        i++;
+    }else{init();}
+    
+}
+window.onload = init();
